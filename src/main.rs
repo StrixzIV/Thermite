45d1267c165b14e9c105162236fcb3871e505d04
Jirayu Kaewsing <strixz.self@gmail.com>
2023-03-30 20:05:28 +0700

    Create HTTPRequest struct

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,28 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::{ TcpListener, TcpStream };
 
+struct HTTPRequest<'a> {
+    method: &'a str,
+    path: &'a str
+}
+
 fn handle_connection(mut stream: TcpStream) {
 
     let mut buffer = [0; 1024];
-
+    
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    
+    let raw_request = String::from_utf8_lossy(&buffer[..]);
+    let request = raw_request.split("\r\n").collect::<Vec<&str>>()[0];
+    
+    let req = HTTPRequest {
+        method: request.split(" ").collect::<Vec<&str>>()[0].trim(),
+        path: request.split(" ").collect::<Vec<&str>>()[1].trim()
+    };
 
-    let get = b"GET / HTTP/1.1\r\n";
+    println!("{}", format!("Request: {}, Path: {}", req.method, req.path));
 
-    let (status, filename) = if buffer.starts_with(get) {("HTTP/1.1 200 OK", "index.html")} else {("HTTP/1.1 404 NOT FOUND", "404.html")};
+    let (status, filename) = if req.method == "GET" && req.path == "/" {("HTTP/1.1 200 OK", "index.html")} else {("HTTP/1.1 404 NOT FOUND", "404.html")};
 
     let contents = fs::read_to_string(format!("resources/{filename}")).unwrap();
     let response = format!(
