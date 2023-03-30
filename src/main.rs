40d1ac297eb6f893f3882f8a7a2bdd67ffbb6f3a
Jirayu Kaewsing <strixz.self@gmail.com>
2023-03-30 19:04:41 +0700

    Adding response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{ TcpListener, TcpStream };
 
+fn handle_connection(mut stream: TcpStream) {
+
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+}
 
 fn main() {
     
@@ -9,7 +22,7 @@ fn main() {
     for stream in listener.incoming() {
 
         let stream = stream.unwrap();
-        println!("Connection established.");
+        handle_connection(stream)
 
     }
 
